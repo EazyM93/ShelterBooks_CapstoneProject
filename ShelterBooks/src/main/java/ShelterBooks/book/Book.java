594fac67d6e94a321e0005379b4be1a2bde99ba3
EazyM93 <centini.manuel.93@gmail.com>
2023-09-04 23:59:16 +0200

    Creazione Book Entity + Genre Enum

---
+++ ShelterBooks/src/main/java/ShelterBooks/book/Book.java
@@ -0,0 +1,54 @@
+package ShelterBooks.book;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+	@Id
+	@GeneratedValue
+	private UUID idBook;
+	
+	@Column(nullable = false, unique = true)
+	private String isbn;
+	
+	@Column(nullable = false, unique = true)
+	private String title;
+	
+	@Column(nullable = false)
+	private String author;
+	
+	@Column(nullable = false)
+	private String publisher;
+	
+	@Column(nullable = false)
+	private int pages;
+	
+	@Column(nullable = false)
+	private double price;
+	
+	@Column(nullable = false)
+	private int publicationYear;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private BookGenre genre;
+	
+}
