64344307ae683d201614c4b00519b8e479cdc20c
EazyM93 <centini.manuel.93@gmail.com>
2023-09-04 23:08:23 +0200

    Creazione del progetto

---
+++ ShelterBooks/src/main/java/ShelterBooks/ShelterBooksApplication.java
@@ -0,0 +1,13 @@
+package ShelterBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterBooksApplication.class, args);
+	}
+
+}
---
+++ ShelterBooks/src/test/java/ShelterBooks/ShelterBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package ShelterBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
