816bcee18ad5867c716133041fac4793c984e160
EazyM93 <centini.manuel.93@gmail.com>
2023-09-05 11:16:34 +0200

    Scrittura BodyPayload

--- ShelterBooks/src/main/java/ShelterBooks/book/BookPayload.java
+++ ShelterBooks/src/main/java/ShelterBooks/book/BookPayload.java
@@ -1,5 +1,7 @@
 package ShelterBooks.book;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,4 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BookPayload {
 
+	private String isbn;
+	private String title;
+	private String author;
+	private String publisher;
+	private int pages;
+	private double price;
+	private int publicationYear;
+	@Enumerated(EnumType.STRING)
+	private BookGenre genre;
+	private boolean avaliableEbook;
+	private Integer avaliableCopies;
+	private Double ebookSize;
+	
 }
