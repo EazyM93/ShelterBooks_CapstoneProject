250ae668c3b9ec2b3a16b449b81e87b05dc1747d
EazyM93 <centini.manuel.93@gmail.com>
2023-09-05 11:21:46 +0200

    Update BookRepository.java

--- ShelterBooks/src/main/java/ShelterBooks/book/BookRepository.java
+++ ShelterBooks/src/main/java/ShelterBooks/book/BookRepository.java
@@ -1,11 +1,15 @@
 package ShelterBooks.book;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ShelterBooks.user.User;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID>{
-
+	
+	Optional<Book> findById(UUID idBook);
 }
