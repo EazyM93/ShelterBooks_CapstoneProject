de5af1808dfe39a05c56be5d1df53d0215f6ef9c
Manuel Centini <centini.manuel.93@gmail.com>
2023-09-07 13:57:53 +0200

    Create CartPayload

--- ShelterBooks/src/main/java/ShelterBooks/cart/Cart.java
+++ ShelterBooks/src/main/java/ShelterBooks/cart/Cart.java
@@ -1,10 +1,15 @@
 package ShelterBooks.cart;
 
+import java.util.List;
 import java.util.UUID;
 
+import ShelterBooks.book.Book;
+import ShelterBooks.user.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +26,12 @@ public class Cart {
 
 	@Id
 	@GeneratedValue
-	private UUID idBook; 
+	private UUID idBook;
+	
+	@OneToOne
+	private User user;
+	
+	@OneToMany(mappedBy = "cart")
+	private List<Book> books;
 	
 }
---
+++ ShelterBooks/src/main/java/ShelterBooks/cart/CartPayload.java
@@ -0,0 +1,14 @@
+package ShelterBooks.cart;
+
+import ShelterBooks.book.BookGenre;
+import ShelterBooks.book.EbookStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartPayload {
+
+}
