d78eb0a1a3a379200b04a146d4e72266b78e5d7e
Manuel Centini <centini.manuel.93@gmail.com>
2023-09-07 13:59:34 +0200

    User Entity - Fix getAuthorities

--- ShelterBooks/src/main/java/ShelterBooks/user/User.java
+++ ShelterBooks/src/main/java/ShelterBooks/user/User.java
@@ -2,6 +2,7 @@ package ShelterBooks.user;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -64,7 +65,11 @@ public class User implements UserDetails{
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of(new SimpleGrantedAuthority(role.name()));
+		 if (this.role != null) {
+			 return List.of(new SimpleGrantedAuthority(role.name()));
+		 }
+		 
+		 return Collections.emptyList();
 	}
 
 	@Override
