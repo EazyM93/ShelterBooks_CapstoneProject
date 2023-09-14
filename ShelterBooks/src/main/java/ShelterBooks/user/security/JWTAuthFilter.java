b13464ab68f35bc578faf37761505f919f0f9a8a
EazyM93 <centini.manuel.93@gmail.com>
2023-09-14 11:35:42 +0200

    Filter Update For Authorize endpoint

--- ShelterBooks/src/main/java/ShelterBooks/user/security/JWTAuthFilter.java
+++ ShelterBooks/src/main/java/ShelterBooks/user/security/JWTAuthFilter.java
@@ -26,6 +26,14 @@ public class JWTAuthFilter extends OncePerRequestFilter{
 	
 	@Autowired
 	UserService us;
+	
+	private static final String[] PUBLIC_ROUTES = { 
+			"/books" // GET books
+	};
+	
+	private static final String[] ADMIN_ROUTES = { 
+			"/books" // POST create a book
+	};
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
@@ -33,26 +41,43 @@ public class JWTAuthFilter extends OncePerRequestFilter{
 			FilterChain filterChain)throws ServletException, IOException {
 		
 		String authHeader = request.getHeader("Authorization");
-
+		String servletPath = request.getServletPath();
+		
+		if (isPublicRoute(servletPath) && (request.getMethod().equals("GET")
+				|| (request.getMethod().equals("POST") && "/reviews".equals(servletPath)))) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
 		if (authHeader == null || !authHeader.startsWith("Bearer "))
 			throw new UnauthorizedException("Per favore passa il token nell'authorization header");
 		
 		String token = authHeader.substring(7);
 		
-		System.out.println("TOKEN -------> " + token);
+		try {
+			
+			System.out.println("TOKEN -------> " + token);
 
-		jwttools.verifyToken(token);
+			jwttools.verifyToken(token);
 
-		String id = jwttools.extractSubject(token);
+			String id = jwttools.extractSubject(token);
 		
-		User currentUser = us.findById(UUID.fromString(id));
+			User currentUser = us.findById(UUID.fromString(id));
 
-		UsernamePasswordAuthenticationToken authToken = 
-				new UsernamePasswordAuthenticationToken(currentUser, null, currentUser.getAuthorities());
+			if (isAdminRoute(servletPath) && !"ADMIN".equalsIgnoreCase(currentUser.getRole().toString())) {
+				throw new UnauthorizedException("L'accesso a questa route richiede il ruolo di admin");
+			}
+			
+			UsernamePasswordAuthenticationToken authToken = 
+					new UsernamePasswordAuthenticationToken(currentUser, null, currentUser.getAuthorities());
 
-		SecurityContextHolder.getContext().setAuthentication(authToken);
+			SecurityContextHolder.getContext().setAuthentication(authToken);
 
-		filterChain.doFilter(request, response);
+			filterChain.doFilter(request, response);
+			
+		} catch (Exception e) {
+		}
+		
 
 	}
 
@@ -60,6 +85,26 @@ public class JWTAuthFilter extends OncePerRequestFilter{
 	protected boolean shouldNotFilter(HttpServletRequest request) {
 		System.out.println(request.getServletPath());
 		return new AntPathMatcher().match("/auth/**", request.getServletPath());
+	}
+	
+	// Verifica se la route è pubblica per le richieste GET
+	private boolean isPublicRoute(String servletPath) {
+		for (String publicRoute : PUBLIC_ROUTES) {
+			if (new AntPathMatcher().match(publicRoute, servletPath)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Verifica se la route richiede il ruolo di admin
+	private boolean isAdminRoute(String servletPath) {
+		for (String adminRoute : ADMIN_ROUTES) {
+			if (new AntPathMatcher().match(adminRoute, servletPath)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
