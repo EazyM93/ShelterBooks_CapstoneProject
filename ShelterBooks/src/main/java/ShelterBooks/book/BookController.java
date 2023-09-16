4ec662735abba5c442aaab802dd2b5aa4315710b
EazyM93 <centini.manuel.93@gmail.com>
2023-09-16 02:30:02 +0200

    Update BookController.java

--- ShelterBooks/src/main/java/ShelterBooks/book/BookController.java
+++ ShelterBooks/src/main/java/ShelterBooks/book/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,9 @@ public class BookController {
 	}
 	
 	// --------------------------------------------------------get book by id
-	public Book findById(UUID idBook) {
+	@GetMapping("/idBook/{idBook}")
+	@PreAuthorize("hasAuthority('ADMIN')")
+	public Book findById(@PathVariable UUID idBook) {
 		return bookService.findById(idBook);
 	}
 	
